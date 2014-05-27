0114d16a3c3c6fab0dd35e7c88f5cb1e39994fa5
danquimby <danquimby@gmail.com>
2014-05-27 18:31:16 +0400

    update
    Create function for work to class game block.

--- objects.cpp
+++ objects.cpp
@@ -1,24 +1,35 @@
 #include "objects.h"
 
-GameBlock::GameBlock() {
-    m_pHGE = DQ::cloneHGE();
+GameBlock::GameBlock(const GameBlockType _type, const DQ::PointI& _ptGrid) {
+    m_ptPostionOnGrid = _ptGrid;
 
+    m_pHGE = DQ::cloneHGE();
 // If global texture don't load, then load image.
-    if (!m_nTiledTexture)
+    if (!g_nTiledTexture)
 #ifdef _DEBUG
-        m_pHGE->Texture_Load("../data/tiled.png");
+        g_nTiledTexture = m_pHGE->Texture_Load("../data/tiled.png");
 #else
-        m_pHGE->Texture_Load("data/tiled.png");
+        g_nTiledTexture = m_pHGE->Texture_Load("data/tiled.png");
 #endif
+    _init(_type);
 
 }
 
 GameBlock::~GameBlock() {
+    if (!m_pBlockImage)
+        delete m_pBlockImage;
+}
+
+void GameBlock::setPositionObject(const DQ::PointI& _newGrid) {
+    m_ptPostionOnGrid = _newGrid;
+}
+
+void GameBlock::moveObject(const DQ::PointI& _vecDirection) {
 
 }
 
 void GameBlock::Render() {
-
+    m_pBlockImage->Render(0,0);
 }
 
 void GameBlock::Process() {
@@ -28,6 +39,13 @@ void GameBlock::Process() {
 DQ::RectF GameBlock::area() const {
     return m_rtAreaBlock;
 }
+
 void GameBlock::setArea(const DQ::RectF& _area) {
     m_rtAreaBlock = _area;
+}
+
+void GameBlock::_init(const GameBlockType _type) {
+    if (_type > GAME_BLOCK_MAX) return;
+
+    m_pBlockImage = new hgeSprite(g_nTiledTexture,(float) (_type % 10) * 64,(float) (_type / 10) * 64,64,64);
 }
