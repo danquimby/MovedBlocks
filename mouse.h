c1ed1159cbf3bb399d697a3973eb07671d1197a1
danquimby <danquimby@gmail.com>
2014-05-27 21:06:07 +0400

    update
    Make test object and push to scene.

--- mouse.h
+++ mouse.h
@@ -27,7 +27,7 @@ public:
     void setMouseType(const MouseType );
 
     // @override method for render to scene.
-    void Render();
+    void Render(const DQ::PointF& = DQ::PointF());
 
     // @override method for processing object.
     void Process();
--- window.cpp
+++ window.cpp
@@ -1,15 +1,16 @@
 #include "window.h"
 
-GameBlock*  block;
+GameBlock* block;
 
 Window::Window() {
     g_pApplicationCursor = new Mouse;
     block = new GameBlock(GAME_BLOCK_TREE1);
+    block->setPositionObject(DQ::PointI(1,0));
 }
 Window::~Window() {
 }
 
-void Window::Render() {
+void Window::Render(const DQ::PointF& _ptOffset) {
     block->Render();
     g_pApplicationCursor->Render();
 }
